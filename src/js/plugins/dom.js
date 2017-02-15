485154a1262e1487ac51e4c66c75e997bdb572ea
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2017-02-15 11:19:35 +0100

    Add required option to pass a DOM handler

--- src/js/plugins/dom.js
+++ src/js/plugins/dom.js
@@ -1,12 +1,14 @@
-import VeamsQuery from 'veams-query';
-
 const VeamsDOM = {
 	pluginName: '$',
-	initialize: function (Veams) {
+	initialize: function (Veams, opts) {
+		if(!opts || !opts.DOM) {
+			console.error('VeamsDOM :: You need to pass an options object with a DOM handler: options.DOM!');
+			return;
+		}
 		if (Veams.$) {
 			console.log('VeamsDOM :: It seems that you have already defined a DOM handler. I am overwriting it now for you ;)');
 		}
-		Veams.$ = VeamsQuery;
+		Veams.$ = opts.DOM;
 	}
 };
 
